0017d60e259c93ce3d4bc6eb21ec76f9d9b4a289
kondim23 <kondim23@gmail.com>
2021-12-05 22:17:20 +0200

    implemented non-tested: discrete frechet distance

--- src/curve.cpp
+++ src/curve.cpp
@@ -29,8 +29,48 @@ double continuous_frechet(Curve*,Curve*){
 }
 
 //non implemented
-double discrete_frechet(Curve*,Curve*){
+//TODO #4 euclidean or 1d?
+double discrete_frechet(Curve* c1,Curve* c2){
 
-    return 1.0;
+
+    vector<float> curve1 = c1->getCurve(), point1;
+    vector<float> curve2 = c2->getCurve(), point2;
+    unsigned int m1,m2;
+    double minPrev;
+
+    for (m1=0 ; m1 < curve1.size() and curve1.at(m1) != PADDING_VALUE ; m1++){}
+    for (m2=0 ; m2 < curve2.size() and curve1.at(m2) != PADDING_VALUE ; m2++){}
+
+    double dynamic[m1][m2];
+
+    for (int i=0 ; i<m1 ; i++){
+
+        point1 = vector<float>(i+1,curve1.at(i));
+        point2 = vector<float>(1,curve2.at(0));
+        
+        dynamic[i][0] = calculate_distance(EUCLIDEAN,point1,point2);
+    }
+
+    for (int i=1 ; i<m2 ; i++){
+
+        point2 = vector<float>(i+1,curve2.at(i));
+        point1 = vector<float>(1,curve1.at(0));
+        
+        dynamic[0][i] = calculate_distance(EUCLIDEAN,point1,point2);
+    }
+
+    for (int i=1 ; i<m1 ; i++){
+        for (int j=1 ; j<m2 ; j++){
+
+            point1 = vector<float>(i+1,curve1.at(i));
+            point2 = vector<float>(i+1,curve2.at(j));
+
+            minPrev = min(dynamic[i-1][j],min(dynamic[i-1][j-1],dynamic[i][j-1]));
+
+            dynamic[i][j] = max(minPrev,calculate_distance(EUCLIDEAN,point1,point2));
+        }
+    }
+
+    return dynamic[m1-1][m2-1];
 }
 
